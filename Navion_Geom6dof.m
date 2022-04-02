a4c8f044e0c44f46839852ff89b9fb8964d3a389
YANGX5 <102183440+YANGX5@users.noreply.github.com>
2022-04-01 21:38:48 -0400

    Add files via upload
    Corrected DATUM, AC, and CG location; changed propeller disc coordinates.

--- Navion_Geom6dof.m
+++ Navion_Geom6dof.m
@@ -1,4 +1,4 @@
-clear all
+clear
 clc
 
 %Trim Velocity components
@@ -63,8 +63,20 @@ b=44;%
 cbar = 4+2/12;%page 26
 weight = 3695;%
 
-cg=[0 0 0];
-%Page 278 7.9550ft off datum
+%DATUM 5.3769 [ft] fwd of stub wing rib
+
+%Scaling factor Page 38 is 11.0628141. Calculate the AC location wrt DATUM
+sweep_i=10.66*pi/180; sweep_o=1*pi/180; TR_i=0.67552; TR_o=0.72176; y_b=6.46166;
+Si=33.16423; So=54.87255; C_bar=4.34761;
+yMAC_i=1/3*((1+2*TR_i)/(1+TR_i))*b/2;yMAC_o=1/3*((1+2*TR_o)/(1+TR_o))*b/2;
+yMAC=(yMAC_i*Si+(y_b+yMAC_o)*So)/(Si+So);
+xLE=((yMAC_i*tan(sweep_i))*Si+((y_b*tan(sweep_i)+yMAC_o*tan(sweep_o))*So))/(Si+So);
+xAC=(xLE+C_bar/4);%in Ft, assumeing .25 chord
+
+xCG=7.9550-5.3769-xAC;
+%Page 278 7.955ft off datum,make AC the new DATUM point,modefy the POH [x]
+%Negative value is in front of the AC, Positive is aft of AC
+cg=[xCG 0 0];
 
 ac=[0 0 0];
 
@@ -88,7 +100,7 @@ control = [elevator, aileron, rudder, throtle];
 %Left
 Vect_L=[0.998,-0.057,0.017];%
 %Toe Out 3.25deg, pitch down by 1 deg
-Pos_L=[95.46/12-3.882,5.1875,0];%
+Pos_L=[-3.6507-xAC,5.1875,0];%position of propeller disc
 %Right
 Vect_R=[0.998,0.057,0.017];%
-Pos_R=[95.46/12,-5.1875,0];%
+Pos_R=[-3.6507-xAC,-5.1875,0];%
